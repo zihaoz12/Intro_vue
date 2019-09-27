b9c5dc38664cb0b599cd1100c7e99a3e48d53803
zihaoz12 <zzhaa12@gmail.com>
2019-09-26 23:29:33 -0700

    add todo-list

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
-    Hello World
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-
+import Todos from './components/Todo/Todos.vue';
 
 export default {
   name: 'app',
   components: {
-    
+    Todos
+  },
+  data(){
+    return{
+      todos:[
+        {
+          id:1,
+          title:"Todo One",
+          completed:false
+        },
+        {
+          id:2,
+          title:"Todo Two",
+          completed:true
+        },
+        {
+          id:3,
+          title:"Todo Three",
+          completed:false
+        },
+      ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo=> todo.id !== id);
+    }
   }
 }
 </script>
