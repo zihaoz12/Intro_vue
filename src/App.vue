b671ad1f52e025bf35f2a27ab5335b31ab94ec41
zihaoz12 <zzhaa12@gmail.com>
2019-09-27 00:03:18 -0700

    simple todo list finished

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div id="app">
+    <Header/>
+    <AddTodo v-on:add-Todo="addTodo"/>
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
+import Header from './components/Todo/Header';
 import Todos from './components/Todo/Todos.vue';
+import AddTodo from './components/Todo/AddTodo.vue';
 
 export default {
   name: 'app',
   components: {
+    Header,
+    AddTodo,
     Todos
   },
   data(){
@@ -36,11 +42,33 @@ export default {
   methods:{
     deleteTodo(id){
       this.todos = this.todos.filter(todo=> todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo]
     }
   }
 }
 </script>
 
 <style>
-
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding:0;
+  }
+  body{
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
+  .btn{
+    display: inline-block;
+    border: none;
+    background: #555;
+    color:#fff;
+    padding:7px 20px;
+    cursor: pointer;
+  }
+  .btn:hover{
+    background: #666;
+  }
 </style>
